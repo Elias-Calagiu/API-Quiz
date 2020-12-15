ff41e40bd1579e942f5f57a78b6ebae9bd874cab
Elias Calagiu <eliasc81@gmail.com>
2020-12-14 16:33:18 -0800

    declared first variable with questions array

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("yoyo");
+var question1 = [{
+    text: "How many months have 28 days?", 
+    choices: ["1", "6", "9", "12",],
+    answer: "12"},{
+    text: "whos the prez?", 
+    choices: ["dog", "cat", "ya boy", "trump",],
+    answer: "trump"}
+]
+
